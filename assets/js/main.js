811a499c72eb039ff2b5011661266f368c04db5d
Russell Omar <tom.jhon.1992@gmail.com>
2022-10-03 03:56:06 +0900

    error correction
    error correction

--- assets/js/main.js
+++ assets/js/main.js
@@ -41,7 +41,7 @@ var tooltipList = tooltipTriggerList.map(function (tooltipTriggerEl) {
 
     
     /* Github Calendar - https://github.com/IonicaBizau/github-calendar */
-    new GitHubCalendar("#github-graph", "RussellOmar", { responsive: true });
+    new GitHubCalendar("#github-graph", "Russell", { responsive: true });
     
     
     /* Github Activity Feed - https://github.com/caseyscarborough/github-activity */
